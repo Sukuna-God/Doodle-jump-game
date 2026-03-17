113dc1d3529918f40623d4299223179456ccbbbb
Owner <Owner@DESKTOP-QMELFFR>
2026-03-17 22:48:59 +0530

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <raylib.h>
 
+static int posX;
+static int posY;
+
 int main(){
     std::cout<<"Doodle Game!"<<std::endl;
 
-    InitWindow(800, 600, "Doodle Game!");
+    InitWindow(400, 600, "Doodle Game!");
+
+    Texture2D doodle = LoadTexture("assets/doodle.png");
 
     SetTargetFPS(60);
 
+    posX = 400;
+    posY = 300;
     while(!WindowShouldClose()){
+
+        
+
         BeginDrawing();
 
         ClearBackground(BLUE);
+
+        DrawTexture(doodle, 200, 300, WHITE);
 
         EndDrawing();
 
